359b6ff3c30d06877a78af85901a68f277fa1285
test <test@tests-MacBook-Pro.local>
2018-08-27 17:10:46 -0500

    First exercise

---
+++ src/introductory/exercisesClass.java
@@ -0,0 +1,25 @@
+package introductory;
+
+public class exercisesClass {
+    int n;
+    String asterisk="*";
+
+    public exercisesClass(){}
+
+    public exercisesClass(int n) {
+        this.n = n;
+    }
+
+    //Triangle Exercises
+
+    public void exerciseOne(){
+        System.out.println("Print one asterisk\n"+asterisk+"\n");
+    }
+
+    public void drawHorinzontalLine(){}
+
+    public void drawVerticalLine(){}
+
+    public void drawRightTriangle(){}
+
+}
---
+++ src/introductory/exercisesMain.java
@@ -0,0 +1,21 @@
+package introductory;
+
+public class exercisesMain {
+    public static void main (String[] args){
+        //Setting variables
+        int n;
+        String asterisk="*";
+        exercisesClass exercise;
+
+        //Exercises One
+        exercise = new exercisesClass();
+        exercise.exerciseOne();
+
+
+        //Get value of n
+        System.out.println("Give value of n: ");
+
+
+
+    }
+}
